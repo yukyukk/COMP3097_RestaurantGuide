ae612b0095b2e2f842df6e36abb5584ba2a445a1
ljingGBC <lei.jing@georgebrown.ca>
2021-03-29 10:05:54 -0400

    add CoreDataManager

---
+++ RestaurantGuide/MainViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RestaurantGuide
+//
+//  Created by Tech on 2021-03-28.
+//  Copyright © 2021 GBC. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //CoreDataManager.storeObj()
+        
+        CoreDataManager.fetchObj()
+        
+        // Do any additional setup after loading the view.
+        
+        
+    }
+
+
+}
+
